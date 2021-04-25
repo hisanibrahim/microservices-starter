ad5864a892025d2cb349051fae88692716cde406
Hisan Ibrahim <hisanibrahim@gmail.com>
2021-04-25 13:17:06 +0530

    connected posts service

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default () => {
+  const [title, setTitile] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+    setTitile("");
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
-            <label>Title</label>
-            <input className="form-control" />
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitile(e.target.value)}
+            className="form-control"
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
