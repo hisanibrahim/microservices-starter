0232b49e69c2a26aca3598781ff95c72a475709c
Hisan Ibrahim <hisanibrahim@gmail.com>
2021-05-04 22:49:51 +0530

    moderation status in client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,20 @@ import React from "react";
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    switch (comment.status) {
+      case "APPROVED":
+        content = comment.content;
+        break;
+      case "PENDING":
+        content = "This comment is awaiting moderation";
+        break;
+      case "REJECTED":
+        content = "This comment is rejected";
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
